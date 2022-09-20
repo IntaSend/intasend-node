0f689b8c104e6108fb4a24761125d34ce5f97d19
felixcheruiyot <felix@intasend.com>
2022-09-20 16:50:41 +0300

    2 files modified

--- collection.ts
+++ collection.ts
@@ -3,14 +3,14 @@ import RequestClient from './requests';
 class Collection extends RequestClient {
   charge(payload: Object) {
     this.secret_key = '';
-    return this.send(payload, '/api/v1/checkout/');
+    return this.send(payload, '/api/v1/checkout/', 'POST');
   }
 
   mpesaStkPush(payload: Object) {
     this.secret_key = '';
     payload['method'] = 'M-PESA';
     payload['currency'] = 'KES';
-    return this.send(payload, '/api/v1/payment/collection/');
+    return this.send(payload, '/api/v1/payment/collection/', 'POST');
   }
 }
 
--- payouts.ts
+++ payouts.ts
@@ -4,7 +4,7 @@ import RequestClient from './requests';
 
 class Payouts extends RequestClient {
   initiate(payload: Object) {
-    return this.send(payload, '/api/v1/send-money/initiate');
+    return this.send(payload, '/api/v1/send-money/initiate', 'POST');
   }
 
   sign(nonce) {
@@ -21,11 +21,11 @@ class Payouts extends RequestClient {
       }
       payload['nonce'] = this.sign(payload['nonce']);
     }
-    return this.send(payload, '/api/v1/send-money/approve/');
+    return this.send(payload, '/api/v1/send-money/approve/', 'POST');
   }
 
   status(payload: Object) {
-    return this.send(payload, '/api/v1/send-money/status/');
+    return this.send(payload, '/api/v1/send-money/status/', 'POST');
   }
 }
 
