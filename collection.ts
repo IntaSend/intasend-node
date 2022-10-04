4456bfe41927623f025321282131830be0ab0d54
FELIX CHERUIYOT <felix@intasend.com>
2022-10-04 14:01:24 +0300

    Added payment status check method

--- collection.ts
+++ collection.ts
@@ -12,6 +12,20 @@ class Collection extends RequestClient {
     payload['currency'] = 'KES';
     return this.send(payload, '/api/v1/payment/collection/', 'POST');
   }
+  
+  status(invoiceID: String, checkoutID: String = None, signature:String = None) {
+    payload = {
+      "invoice_id": invoiceID,
+    }
+    if (checkout_id and signature) {
+      payload = {
+        "invoice_id": invoiceID,
+        "signature": signature,
+        "checkout_id": checkoutID
+      }
+    }
+    return this.send(payload, '/api/v1/payment/status/', 'POST');
+  }
 }
 
 export default Collection;
