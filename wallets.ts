343a64da82ef621a7a136d45de32dea875fe213f
FELIX CHERUIYOT <felix@intasend.com>
2022-10-04 15:42:58 +0300

    Added parameters for IntraTransfer

--- wallets.ts
+++ wallets.ts
@@ -8,8 +8,13 @@ class Wallet extends RequestClient {
     return this.send(payload, '/api/v1/wallets/', 'POST');
   }
 
-  intraTransfer(payload: Object) {
-    return this.send(payload, '/api/v1/wallets/', 'POST');
+  intraTransfer(sourceID: string, destinationID: string, amount: float, narrative: string) {
+    let payload = {
+      'wallet_id': destinationID,
+      'amount': amount,
+      'narrative': narrative
+    }
+    return this.send(payload, `/api/v1/wallets/${sourceID}/intra_transfer/`, 'POST');
   }
 
   get(payload: Object) {
