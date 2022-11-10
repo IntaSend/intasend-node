3965a578ed2a94f44dd6e704c484793fcf297e55
felixcheruiyot <felix@intasend.com>
2022-11-10 13:39:16 +0300

    Bump version and added payouts helper methods

--- payouts.js
+++ payouts.js
@@ -7,15 +7,33 @@ class Payouts extends RequestClient {
     return this.send(payload, '/api/v1/send-money/initiate/', 'POST');
   }
 
+  mpesa(payload) {
+    payload['provider'] = 'MPESA-B2C';
+    return this.initiate(payload);
+  }
+
+  mpesaB2B(payload) {
+    payload['provider'] = 'MPESA-B2B';
+    return this.initiate(payload);
+  }
+
+  bank(payload) {
+    payload['provider'] = 'PESALINK';
+    return this.initiate(payload);
+  }
+
+  intasend(payload) {
+    payload['provider'] = 'INTASEND';
+    return this.initiate(payload);
+  }
+
   sign(nonce) {
-	  console.log("nonce", nonce)
     const data = Buffer.from(nonce);
     const sign = crypto.sign('SHA256', data, this.private_key);
     return sign.toString('hex');
   }
 
   approve(payload, sign_nonce) {
-	  console.log("tosig", payload['nonce'])
     payload['sign_nonce'] = sign_nonce;
     if (sign_nonce) {
       if (!this.private_key) {
