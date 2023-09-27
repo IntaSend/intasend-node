8ee3b019648851d1cae50a800b71d89e21b79f24
Mugendi Gitonga <gitongav97@gmail.com>
2023-09-27 08:31:48 +0300

    Removed null and pass an empty object for payload in GET requests

--- wallets.js
+++ wallets.js
@@ -2,7 +2,7 @@ const RequestClient = require('./requests');
 
 class Wallet extends RequestClient {
   list() {
-    return this.send(null, '/api/v1/wallets/', 'GET');
+    return this.send({}, '/api/v1/wallets/', 'GET');
   }
   create(payload) {
     return this.send(payload, '/api/v1/wallets/', 'POST');
@@ -22,11 +22,11 @@ class Wallet extends RequestClient {
   }
 
   get(walletID) {
-    return this.send(null, `/api/v1/wallets/${walletID}/`, 'GET');
+    return this.send({}, `/api/v1/wallets/${walletID}/`, 'GET');
   }
 
   transactions(walletID) {
-    return this.send(null, `/api/v1/wallets/${walletID}/transactions/`, 'GET');
+    return this.send({}, `/api/v1/wallets/${walletID}/transactions/`, 'GET');
   }
 
   fundMPesa(payload, walletID) {
